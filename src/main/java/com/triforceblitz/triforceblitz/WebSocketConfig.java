7c32f58c3640587fdd76399bf34a2500a77630e8
Jesse Gerard Brands <jessebrands@icloud.com>
2023-11-15 15:17:36 +0100

    Added WebSocket/STOMP dependencies

---
+++ src/main/java/com/triforceblitz/triforceblitz/WebSocketConfig.java
@@ -0,0 +1,37 @@
+package com.triforceblitz.triforceblitz;
+
+import com.triforceblitz.triforceblitz.websocket.SessionHandshakeInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.lang.Nullable;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeFailureException;
+import org.springframework.web.socket.server.HandshakeHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.util.Map;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // TODO: We should definitely not be allowing all origins!
+        registry.addEndpoint("/ws")
+                .addInterceptors(new SessionHandshakeInterceptor())
+                .setAllowedOrigins("*");
+    }
+}
