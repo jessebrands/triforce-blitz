c3e32d57fc022881ed2fbcd90214e20fc15fc738
Jesse Gerard Brands <jesse@jgbrands.nl>
2025-02-10 21:52:28 +0100

    python: add processBuilder() method to Interpreter

--- src/main/java/com/triforceblitz/triforceblitz/python/Interpreter.java
+++ src/main/java/com/triforceblitz/triforceblitz/python/Interpreter.java
@@ -1,6 +1,8 @@
 package com.triforceblitz.triforceblitz.python;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Interpreter {
     private final Path path;
@@ -11,5 +13,13 @@ public class Interpreter {
 
     public Path getPath() {
         return path;
+    }
+
+    public ProcessBuilder processBuilder(String entrypoint, String... args) {
+        var command = new ArrayList<String>();
+        command.add(path.toString());
+        command.add(entrypoint);
+        command.addAll(List.of(args));
+        return new ProcessBuilder(command);
     }
 }
