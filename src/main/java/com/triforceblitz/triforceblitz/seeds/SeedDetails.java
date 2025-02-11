ef7e73a1856cb319c403996052064b40872d9c39
Jesse Gerard Brands <jesse@jgbrands.nl>
2025-02-11 15:49:38 +0100

    seeds: add generatedAt to SeedDetails

--- src/main/java/com/triforceblitz/triforceblitz/seeds/SeedDetails.java
+++ src/main/java/com/triforceblitz/triforceblitz/seeds/SeedDetails.java
@@ -1,7 +1,9 @@
 package com.triforceblitz.triforceblitz.seeds;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.time.Instant;
 import java.util.UUID;
 
@@ -81,5 +83,11 @@ public class SeedDetails {
 
     public boolean isGenerated() {
         return Files.exists(getPatchFile());
+    }
+
+    public Instant getGeneratedAt() throws IOException {
+        var attrs = Files.readAttributes(getPatchFile(), BasicFileAttributes.class);
+        var creationTime = attrs.creationTime();
+        return creationTime.toInstant();
     }
 }
