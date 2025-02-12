6e84e75c4fe84eb25c76e2a1a39283537a89a73b
Jesse Gerard Brands <jesse@jgbrands.nl>
2025-02-12 15:03:23 +0100

    seeds/racetime: add RacetimeLock entity

---
+++ src/main/java/com/triforceblitz/triforceblitz/seeds/racetime/RacetimeLock.java
@@ -0,0 +1,62 @@
+package com.triforceblitz.triforceblitz.seeds.racetime;
+
+import com.triforceblitz.triforceblitz.seeds.Seed;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "racetime_locks")
+public class RacetimeLock {
+    @Id
+    @Column(name = "id", nullable = false)
+    private final UUID id;
+
+    @Column(name = "race_slug", length = 32, unique = true, nullable = false)
+    private String raceSlug;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false, cascade = CascadeType.ALL)
+    @JoinColumn(name = "seed_id", referencedColumnName = "id", unique = true, nullable = false)
+    private Seed seed;
+
+    @Column(name = "locked", nullable = false)
+    private boolean locked = true;
+
+    protected RacetimeLock() {
+        this.id = null;
+    }
+
+    public RacetimeLock(String raceSlug, Seed seed) {
+        this.id = UUID.randomUUID();
+        this.raceSlug = raceSlug;
+        this.seed = seed;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getRaceSlug() {
+        return raceSlug;
+    }
+
+    public void setRaceSlug(String raceSlug) {
+        this.raceSlug = raceSlug;
+    }
+
+    public Seed getSeed() {
+        return seed;
+    }
+
+    public void setSeed(Seed seed) {
+        this.seed = seed;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+}
--- src/main/resources/db/changelog/0001_create_seeds.sql
+++ src/main/resources/db/changelog/0001_create_seeds.sql
@@ -19,3 +19,18 @@ create table seeds
     created_at         timestamp          not null default now(),
     constraint seeds_pk primary key (id)
 );
+
+--changeset jessebrands:0001_create_racetime_locks
+create domain racetime_race_slug as varchar(32)
+    check (value ~* '^[a-z0-9]+-[a-z0-9]+-[0-9]{4}$');
+
+create table racetime_locks
+(
+    id        uuid               not null default uuid_generate_v4(),
+    seed_id   uuid               not null,
+    race_slug racetime_race_slug not null,
+    constraint racetime_locks_pk primary key (id),
+    constraint racetime_locks_fk_seed foreign key (seed_id) references seeds (id)
+        on delete cascade on update cascade,
+    constraint racetime_locks_uk_race unique (race_slug)
+);
